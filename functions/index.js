560eef83463a40110da138ba99c91957180a8ff3
sandeep reddy <sandeepreddyr12@gmail.com>
2023-08-06 22:04:10 +0530

    added function to modify/merge contest-entries

--- functions/index.js
+++ functions/index.js
@@ -1,55 +1,118 @@
+/* eslint-disable max-len */
 /* eslint-disable no-constant-condition */
 const {initializeApp} = require("firebase-admin/app");
-const {
-  onDocumentCreated,
-  onDocumentWritten,
-  onDocumentUpdated,
-  onDocumentDeleted,
-  Change,
-  FirestoreEvent,
-} = require("firebase-functions/v2/firestore");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 const {
   getFirestore,
-  Timestamp,
+  // Timestamp,
+  // FieldValue,
   FieldValue,
 } = require("firebase-admin/firestore");
+const {setGlobalOptions} = require("firebase-functions/v2");
 // const logger = require("firebase-functions/logger");
+
+setGlobalOptions({maxInstances: 10});
 
 initializeApp();
 const db = getFirestore();
 
-// eslint-disable-next-line max-len
-exports.writetofirestore = onDocumentCreated("users/abc/def/{userId}", (event) => {
-  const snapshot = event.data;
+exports.moralisEventsMerge = onDocumentCreated(
+    "moralis/events/Allcontests/{userId}",
+    (event) => {
+      const snapshot = event.data;
 
-  if (!snapshot) {
-    // console.log("No data associated with the event");
-    return;
-  }
+      if (!snapshot) {
+      // logger.log("snapshot- No data associated with the event");
+        return;
+      }
 
-  const data = snapshot.data();
-  // console.log(data);
+      const data = snapshot.data();
 
-  // // eslint-disable-next-line no-constant-condition
-  // if (data.name === "Ongoing_Contests") {
-  //   console.log("Not the doc looking for");
-  //   return;
-  // }
+      // logger.log("data-", data);
 
-  if (data.name === "Contest_Status") {
-    db.collection("users/abc/def")
-        .where("matchId", "==", data.matchId)
-        .where("name", "==", "Ongoing_Contests")
-        .get()
-        .then((snap) => {
-          snap.forEach((doc) => {
-            // {https://firebase.google.com/docs/firestore/security/rules-fields} partial Writes are never allowed in firestore.
-            doc.ref.update({...data, name: "Ongoing_Contests"});
-            snapshot.ref.delete();
-          });
-        })
-        .catch((err)=>{
-          console.log(err);
-        });
-  }
-});
+      // // eslint-disable-next-line no-constant-condition
+      // if (data.name === "Ongoing_Contests") {
+      //   // console.log("Not the doc looking for");
+      //   return;
+      // }
+
+      if (data.name === "Contest_Status") {
+        return db
+            .collection("moralis/events/Allcontests")
+            .where("matchId", "==", data.matchId)
+            .where("name", "==", "Ongoing_Contests")
+            .get()
+            .then((snap) => {
+              snap.forEach((doc) => {
+                // logger.log("doc", doc.data());
+                delete data.name;
+                delete data.Id;
+                // {https://firebase.google.com/docs/firestore/security/rules-fields} partial Writes are never allowed in firestore.
+                doc.ref.update({...data});
+                snapshot.ref.delete(); // here we are deleteing triggered document.
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+      }
+    },
+);
+
+exports.moralisEnterContestMerge = onDocumentCreated(
+    "moralis/events/Entercontests/{userId}",
+    (event) => {
+      const snapshot = event.data;
+
+      if (!snapshot) {
+      // logger.log("snapshot- No data associated with the event");
+        return;
+      }
+
+      const data = snapshot.data();
+
+      // logger.log("data-", data);
+
+      // // eslint-disable-next-line no-constant-condition
+      // if (data.name === "Ongoing_Contests") {
+      //   // console.log("Not the doc looking for");
+      //   return;
+      // }
+
+      if (data.name === "Enter_Contest") {
+        return db
+            .collection("moralis/events/Entercontests")
+            .where("name", "==", "Enter_Contest")
+            .where("matchId", "==", data.matchId)
+            .where("player", "==", data.player)
+            .get()
+            .then((snap) => {
+              snap.forEach((doc) => {
+                // logger.log("doc", doc.data());
+
+                let deleteFields = {};
+
+                if (doc.data().Id == data.Id && doc.data().value != undefined) {
+                  data[data.team] = data.value;
+                  deleteFields = {team: FieldValue.delete(), value: FieldValue.delete()};
+                } else {
+                  data[data.team] = +data.value + (+doc.data()[data.team] || 0);
+                  snapshot.ref.delete(); // here we are deleteing triggered document.
+                }
+                delete data.Id;
+                delete data.team;
+                delete data.value;
+
+                // {https://firebase.google.com/docs/firestore/security/rules-fields} partial Writes are never allowed in firestore.
+                doc.ref.update({
+                  ...data,
+                  ...deleteFields,
+                });
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+      }
+    },
+);
